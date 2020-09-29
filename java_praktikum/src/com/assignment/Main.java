fe4e81a4996402a00cb17985c87d575e14e51721
Hiddemaru <72017596+hiddemaru@users.noreply.github.com>
2020-09-29 01:36:41 -0700

    mengupload folder java_praktikum

---
+++ java_praktikum/src/com/assignment/Main.java
@@ -0,0 +1,24 @@
+package com.assignment;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+//      Deklarasi
+        int nim;
+        String nama;
+        String jurusan;
+
+//      Assgnment
+        nim = 1822499651;
+        nama = "Helminda Dewi Marunduri";
+        jurusan = "Teknik Informatika";
+
+//      output
+        System.out.println("Hallo Kawan Kawan");
+        System.out.println("Perkenalkan..");
+        System.out.println("Nama : " + nama);
+        System.out.println("NIM : " + nim);
+        System.out.println("Jurusan : " + jurusan);
+    }
+}
